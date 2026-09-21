4c0033bd6de9d3038df3ab00cb0ad72a55cbe63d
Noman Siddique <nainaquarius@icloud.com>
2026-09-21 22:41:43 +0400

    updated

--- backend/app/vat_records/schema.py
+++ backend/app/vat_records/schema.py
@@ -1,4 +1,13 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class VatRecordCreate(BaseModel):
   invoice_number: str
+
+class VatRecordResponse(BaseModel):
+  invoice_number: str
+  product_names: str
+  date: datetime
+  invoice_price: float
+  vat: float
+  total_amount: float
--- backend/app/vat_records/service.py
+++ backend/app/vat_records/service.py
@@ -1,5 +1,5 @@
 from app.database.database import invoices_collection
-from app.vat_records.schema import VatRecordCreate
+from app.vat_records.schema import VatRecordCreate, VatRecordResponse
 from fastapi import HTTPException, status
 
 async def createRecord(data: VatRecordCreate):
@@ -30,9 +30,11 @@ async def createRecord(data: VatRecordCreate):
   vat = invoice_price * 0.05
   total_amount = invoice_price + vat
 
-  return {
-    "product_names": product_names,
-    "invoice_price": invoice_price,
-    "vat": vat,
-    "total_amount": total_amount
-  }
+  return VatRecordResponse(
+    invoice_number=invoice["invoice_number"],
+    product_names=product_names,
+    date=invoice["created_at"],
+    invoice_price=invoice_price,
+    vat=vat,
+    total_amount=total_amount
+  )
